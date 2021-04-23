156cabb972dc32ce68cb3ad24c782a49f8604f75
Paweł Rozenblut <48156138+miodine@users.noreply.github.com>
2021-04-23 12:20:53 +0200

    ult

--- MazeSolver-ultrasound/MazeSolver_ultrasonic/MazeSolver_ultrasonic.ino
+++ MazeSolver-ultrasound/MazeSolver_ultrasonic/MazeSolver_ultrasonic.ino
@@ -1,11 +1,17 @@
-#include <Adafruit_NeoPixel.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "TRSensors.h"
+
+#include <TRSensors.h>
+#include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
+
 #include <Wire.h>
 
 #define ECHO 2 // ultrasonic echo
 #define TRIG 3 // ultrasonic trigger
+
+#define OLED_RESET 9 // OLED display reset pin
+#define OLED_SA0 8   // OLED display set pin
 
 #define PWMA 6  //Left Motor Speed pin (ENA)
 #define AIN2 A0 //Motor-L forward (IN2).
@@ -13,13 +19,108 @@
 #define PWMB 5  //Right Motor Speed pin (ENB)
 #define BIN1 A2 //Motor-R forward (IN3)
 #define BIN2 A3 //Motor-R backward (IN4)
-#define PIN 7
+#define PIN 7 //led pin
 #define NUM_SENSORS 5
 #define OLED_RESET 9
+
 #define OLED_SA0 8
 #define Addr 0x20
 
-// Poniższe dwie funkcje wzięte z repozytorium autorstwa Blagoj Hristov'a
+
+Adafruit_NeoPixel RGB = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800); // setup RGB LEDs
+Adafruit_SSD1306 display(OLED_RESET, OLED_SA0);
+
+
+TRSensors trs = TRSensors();
+unsigned int sensorValues[NUM_SENSORS];
+unsigned int position;
+uint16_t i, j;
+byte value;
+unsigned long lasttime = 0;
+
+void PCF8574Write(byte data);
+byte PCF8574Read();
+uint32_t Wheel(byte WheelPos);
+
+char path[100] = "";
+unsigned char path_length = 0; // the length of the path
+
+
+const int fs_turn_rate = 100; 
+
+
+void PCF8574Write(byte data)
+{
+  Wire.beginTransmission(Addr);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+byte PCF8574Read()
+{
+  int data = -1;
+  Wire.requestFrom(Addr, 1);
+  if (Wire.available())
+  {
+    data = Wire.read();
+  }
+  return data;
+}
+
+
+
+void sort(int *arr, int n){     // function for sorting arrays
+    int tmp;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            if (*(arr + j) < *(arr + i)){
+                tmp = *(arr + i);
+                *(arr + i) = *(arr + j);
+                *(arr + j) = tmp;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+}
+
+char read_infrared(bool verbose=false){
+    byte value;
+                                // declare sensor value as byte type variable
+    PCF8574Write(0xC0 | PCF8574Read());   // write sensor output to PCF8574 I/O expansion module
+    value = PCF8574Read() | 0x3F;          // read PCF module (sensor output)
+
+    if(verbose){    // print result
+        if(value == 0x7F)
+            Serial.println("Left sensor has detected an obstacle.");
+
+        else if(value == 0xBF)
+            Serial.println("Right sensor has detected an obstacle.");
+
+        else if(value == 0x3F)
+            Serial.println("Both sensors have detected an obstacle.");
+
+        else if(value == 0xFF)
+            Serial.println("No obstacle has been detected.");
+    }
+
+    if(value == 0x7F)       // check if left sensor is active
+        return 'L';
+
+    else if(value == 0xBF)  // check if right sensor is active
+        return 'R';
+
+    else if(value == 0x3F)  // check if both sensors are active
+        return 'B';
+
+    else if(value == 0xFF)  // check if no sensors are active
+        return 'N';
+
+    else
+        Serial.println("ERROR!");
+}
 
 void lights(int led_1[3], int led_2[3], int led_3[3], int led_4[3])
 {
@@ -83,19 +184,259 @@ int read_ultrasonic(bool verbose = false)
   return distance;
 }
 
-Adafruit_SSD1306 display(OLED_RESET, OLED_SA0);
 
-TRSensors trs = TRSensors();
-unsigned int sensorValues[NUM_SENSORS];
-unsigned int position;
-uint16_t i, j;
-byte value;
-unsigned long lasttime = 0;
-Adafruit_NeoPixel RGB = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
 
-void PCF8574Write(byte data);
-byte PCF8574Read();
-uint32_t Wheel(byte WheelPos);
+// Path simplification.  The strategy is that whenever we encounter a
+// sequence xBx, we can simplify it by cutting out the dead end.  For
+// example, LBL -> S, because a single S bypasses the dead end
+// represented by LBL.
+void simplify_path()
+{
+  // only simplify the path if the second-to-last turn was a 'B'
+  if (path_length < 3 || path[path_length - 2] != 'B')
+    return;
+
+  int total_angle = 0;
+  int i;
+  for (i = 1; i <= 3; i++)
+  {
+    switch (path[path_length - i])
+    {
+    case 'R':
+      total_angle += 90;
+      break;
+    case 'L':
+      total_angle += 270;
+      break;
+    case 'B':
+      total_angle += 180;
+      break;
+    }
+  }
+
+  // Get the angle as a number between 0 and 360 degrees.
+  total_angle = total_angle % 360;
+
+  // Replace all of those turns with a single one.
+  switch (total_angle)
+  {
+  case 0:
+    path[path_length - 3] = 'S';
+    break;
+  case 90:
+    path[path_length - 3] = 'R';
+    break;
+  case 180:
+    path[path_length - 3] = 'B';
+    break;
+  case 270:
+    path[path_length - 3] = 'L';
+    break;
+  }
+
+  // The path is now two steps shorter.
+  path_length -= 2;
+}
+
+
+//MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY 
+
+
+void SetSpeeds(int Aspeed, int Bspeed)
+{
+  if (Aspeed < 0)
+  {
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+    analogWrite(PWMA, -Aspeed);
+  }
+  else
+  {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+    analogWrite(PWMA, Aspeed);
+  }
+
+  if (Bspeed < 0)
+  {
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+    analogWrite(PWMB, -Bspeed);
+  }
+  else
+  {
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
+    analogWrite(PWMB, Bspeed);
+  }
+}
+
+void m_forward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,LOW);  
+  digitalWrite(BIN2,HIGH); 
+}
+
+void m_backward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void m_right()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void m_left()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,HIGH);  
+}
+
+void m_stop()
+{
+  analogWrite(PWMA,0);
+  analogWrite(PWMB,0);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void m_ninety_right()
+{
+  m_right()
+  delay(50); //adjust
+}
+
+void m_ninety_left()
+{
+  m_right()
+  delay(50); //adjust
+}
+
+
+
+
+
+
+
+
+
+
+
+//MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY MOBILITY 
+
+
+// This function, causes the 3pi to follow a segment of the maze until
+// it detects an intersection, a dead end, or the finish.
+void follow_segment()
+{
+  SetSpeeds()
+  while (1)
+  {
+    if (millis() - lasttime > 100)
+    {
+      if(read_ultrasonic(false) < 4) return;
+    }
+  }
+}
+
+// Code to perform various types of turns according to the parameter dir,
+// which should be 'L' (left), 'R' (right), 'S' (straight), or 'B' (back).
+// The delays here had to be calibrated for the 3pi's motors.
+void turn(unsigned char dir)
+{
+  // if(millis() - lasttime >500)
+  {
+    switch (dir)
+    {
+    case 'L':
+      // Turn left.
+      SetSpeeds(-100, 100);
+      delay(190);
+      break;
+    case 'R':
+      // Turn right.
+      SetSpeeds(100, -100);
+      delay(190);
+      break;
+    case 'B':
+      // Turn around.
+      SetSpeeds(100, -100);
+      delay(400);
+      break;
+    case 'S':
+      // Don't do anything!
+      break;
+    }
+  }
+  SetSpeeds(0, 0);
+  // value = 0;
+  //  while(value != 0xEF)  //wait button pressed
+  //  {
+  //    PCF8574Write(0x1F | PCF8574Read());
+  //    value = PCF8574Read() | 0xE0;
+  //  }
+  Serial.write(dir);
+  Serial.println();
+  lasttime = millis();
+}
+
+// This function decides which way to turn during the learning phase of
+// maze solving.  It uses the variables found_left, found_straight, and
+// found_right, which indicate whether there is an exit in each of the
+// three directions, applying the "left hand on the wall" strategy.
+unsigned char select_turn(unsigned char found_left, unsigned char found_straight, unsigned char found_right)
+{
+  // Make a decision about how to turn.  The following code
+  // implements a left-hand-on-the-wall strategy, where we always
+  // turn as far to the left as possible.
+  if (found_left)
+    return 'L';
+  else if (found_straight)
+    return 'S';
+  else if (found_right)
+    return 'R';
+  else
+    return 'B';
+}
+
+
+
+//CONFIG
+
+// The path variable will store the path that the robot has taken.  It
+// is stored as an array of characters, each of which represents the
+// turn that should be made at one intersection in the sequence:
+//  'L' for left
+//  'R' for right
+//  'S' for straight (going straight through an intersection)
+//  'B' for back (U-turn)
+//
+// Whenever the robot makes a U-turn, the path can be simplified by
+// removing the dead end.  The follow_next_turn() function checks for
+// this case every time it makes a turn, and it simplifies the path
+// appropriately.
+
 
 void setup()
 {
@@ -202,208 +543,6 @@ void setup()
   delay(500);
 }
 
-// This function, causes the 3pi to follow a segment of the maze until
-// it detects an intersection, a dead end, or the finish.
-void follow_segment()
-{
-  int last_proportional = 0;
-  long integral = 0;
-
-  while (1)
-  {
-    // Normally, we will be following a line.  The code below is
-    // similar to the 3pi-linefollower-pid example, but the maximum
-    // speed is turned down to 60 for reliability.
-
-    // Get the position of the line.
-    unsigned int position = trs.readLine(sensorValues);
-
-    // The "proportional" term should be 0 when we are on the line.
-    int proportional = ((int)position) - 2000;
-
-    // Compute the derivative (change) and integral (sum) of the
-    // position.
-    int derivative = proportional - last_proportional;
-    integral += proportional;
-
-    // Remember the last position.
-    last_proportional = proportional;
-
-    // Compute the difference between the two motor power settings,
-    // m1 - m2.  If this is a positive number the robot will turn
-    // to the left.  If it is a negative number, the robot will
-    // turn to the right, and the magnitude of the number determines
-    // the sharpness of the turn.
-    int power_difference = proportional / 20 + integral / 10000 + derivative * 10;
-
-    // Compute the actual motor settings.  We never set either motor
-    // to a negative value.
-    const int maximum = 150; // the maximum speed
-    if (power_difference > maximum)
-      power_difference = maximum;
-    if (power_difference < -maximum)
-      power_difference = -maximum;
-
-    if (power_difference < 0)
-    {
-      analogWrite(PWMA, maximum + power_difference);
-      analogWrite(PWMB, maximum);
-    }
-    else
-    {
-      analogWrite(PWMA, maximum);
-      analogWrite(PWMB, maximum - power_difference);
-    }
-
-    // We use the inner three sensors (1, 2, and 3) for
-    // determining whether there is a line straight ahead, and the
-    // sensors 0 and 4 for detecting lines going to the left and
-    // right.
-    if (millis() - lasttime > 100)
-    {
-      if (sensorValues[1] < 150 && sensorValues[2] < 150 && sensorValues[3] < 150)
-      {
-        // There is no line visible ahead, and we didn't see any
-        // intersection.  Must be a dead end.
-        //  SetSpeeds(0,0);
-        return;
-      }
-      else if (sensorValues[0] > 600 || sensorValues[4] > 600)
-      {
-        // Found an intersection.
-        //  SetSpeeds(0, 0);
-        return;
-      }
-    }
-  }
-}
-
-// Code to perform various types of turns according to the parameter dir,
-// which should be 'L' (left), 'R' (right), 'S' (straight), or 'B' (back).
-// The delays here had to be calibrated for the 3pi's motors.
-void turn(unsigned char dir)
-{
-  // if(millis() - lasttime >500)
-  {
-    switch (dir)
-    {
-    case 'L':
-      // Turn left.
-      SetSpeeds(-100, 100);
-      delay(190);
-      break;
-    case 'R':
-      // Turn right.
-      SetSpeeds(100, -100);
-      delay(190);
-      break;
-    case 'B':
-      // Turn around.
-      SetSpeeds(100, -100);
-      delay(400);
-      break;
-    case 'S':
-      // Don't do anything!
-      break;
-    }
-  }
-  SetSpeeds(0, 0);
-  // value = 0;
-  //  while(value != 0xEF)  //wait button pressed
-  //  {
-  //    PCF8574Write(0x1F | PCF8574Read());
-  //    value = PCF8574Read() | 0xE0;
-  //  }
-  Serial.write(dir);
-  Serial.println();
-  lasttime = millis();
-}
-
-// This function decides which way to turn during the learning phase of
-// maze solving.  It uses the variables found_left, found_straight, and
-// found_right, which indicate whether there is an exit in each of the
-// three directions, applying the "left hand on the wall" strategy.
-unsigned char select_turn(unsigned char found_left, unsigned char found_straight, unsigned char found_right)
-{
-  // Make a decision about how to turn.  The following code
-  // implements a left-hand-on-the-wall strategy, where we always
-  // turn as far to the left as possible.
-  if (found_left)
-    return 'L';
-  else if (found_straight)
-    return 'S';
-  else if (found_right)
-    return 'R';
-  else
-    return 'B';
-}
-
-// The path variable will store the path that the robot has taken.  It
-// is stored as an array of characters, each of which represents the
-// turn that should be made at one intersection in the sequence:
-//  'L' for left
-//  'R' for right
-//  'S' for straight (going straight through an intersection)
-//  'B' for back (U-turn)
-//
-// Whenever the robot makes a U-turn, the path can be simplified by
-// removing the dead end.  The follow_next_turn() function checks for
-// this case every time it makes a turn, and it simplifies the path
-// appropriately.
-char path[100] = "";
-unsigned char path_length = 0; // the length of the path
-
-// Path simplification.  The strategy is that whenever we encounter a
-// sequence xBx, we can simplify it by cutting out the dead end.  For
-// example, LBL -> S, because a single S bypasses the dead end
-// represented by LBL.
-void simplify_path()
-{
-  // only simplify the path if the second-to-last turn was a 'B'
-  if (path_length < 3 || path[path_length - 2] != 'B')
-    return;
-
-  int total_angle = 0;
-  int i;
-  for (i = 1; i <= 3; i++)
-  {
-    switch (path[path_length - i])
-    {
-    case 'R':
-      total_angle += 90;
-      break;
-    case 'L':
-      total_angle += 270;
-      break;
-    case 'B':
-      total_angle += 180;
-      break;
-    }
-  }
-
-  // Get the angle as a number between 0 and 360 degrees.
-  total_angle = total_angle % 360;
-
-  // Replace all of those turns with a single one.
-  switch (total_angle)
-  {
-  case 0:
-    path[path_length - 3] = 'S';
-    break;
-  case 90:
-    path[path_length - 3] = 'R';
-    break;
-  case 180:
-    path[path_length - 3] = 'B';
-    break;
-  case 270:
-    path[path_length - 3] = 'L';
-    break;
-  }
-
-  // The path is now two steps shorter.
-  path_length -= 2;
-}
 
 void loop()
 {
@@ -546,49 +685,5 @@ void loop()
   }
 }
 
-void SetSpeeds(int Aspeed, int Bspeed)
-{
-  if (Aspeed < 0)
-  {
-    digitalWrite(AIN1, HIGH);
-    digitalWrite(AIN2, LOW);
-    analogWrite(PWMA, -Aspeed);
-  }
-  else
-  {
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, HIGH);
-    analogWrite(PWMA, Aspeed);
-  }
 
-  if (Bspeed < 0)
-  {
-    digitalWrite(BIN1, HIGH);
-    digitalWrite(BIN2, LOW);
-    analogWrite(PWMB, -Bspeed);
-  }
-  else
-  {
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, HIGH);
-    analogWrite(PWMB, Bspeed);
-  }
-}
 
-void PCF8574Write(byte data)
-{
-  Wire.beginTransmission(Addr);
-  Wire.write(data);
-  Wire.endTransmission();
-}
-
-byte PCF8574Read()
-{
-  int data = -1;
-  Wire.requestFrom(Addr, 1);
-  if (Wire.available())
-  {
-    data = Wire.read();
-  }
-  return data;
-}
